9052720de0d4a507e28cab17231d9b8df8aa34a4
Dennis Dietrich <d.dietrich84@googlemail.com>
2012-09-13 10:00:12 +0200

    updated composer config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,3 @@
 <?php
 
-spl_autoload_register(function($class)
-{
-    $path = str_replace('\\', DIRECTORY_SEPARATOR, $class);
-    foreach (array('src', 'tests') as $dirPrefix) {
-        $file = __DIR__.'/../'.$dirPrefix.'/'.$path.'.php';
-        if (file_exists($file)) {
-            require_once $file;
-            return true;
-        }
-    }
-});
-
-require_once __DIR__.'/../vendor/Pimple.php';
+require_once __DIR__.'/../vendor/autoload.php';
