a3a8aff7c9c791ab0a97c52eeda758f3453cb622
Dennis Dietrich <d.dietrich84@googlemail.com>
2012-12-13 21:40:32 +0100

    improved flat errors test

--- tests/Rucola/Tests/FormWithErrorsTest.php
+++ tests/Rucola/Tests/FormWithErrorsTest.php
@@ -8,22 +8,32 @@ class FormWithErrorsTest extends \PHPUnit_Framework_TestCase
 {
     public function testFormErrors()
     {
-        $rucula = new Rucola();
+        $rucola = new Rucola();
 
-        $form = $rucula->form('form', array(
-            $rucula->field('username')->nonEmptyText(),
-            $rucula->field('password')->nonEmptyText(),
+        $form = $rucola->form('form', array(
+            $rucola->field('username')->nonEmptyText(),
+            $rucola->field('password')->nonEmptyText(),
+            $rucola->form('address', array(
+                $rucola->field('city')->nonEmptyText(),
+                $rucola->field('street')->nonEmptyText(),
+            )),
         ));
 
         $form->bind(array(
             'username' => '',
             'password' => '',
+            'address' => array(
+                'city' => '',
+                'street' => '',
+            ),
         ));
 
         $form->fold(function ($formWithErrors) {
             $errors = $formWithErrors->getErrorsFlat();
             $this->assertEquals('username', $errors[0]->getField());
             $this->assertEquals('password', $errors[1]->getField());
+            $this->assertEquals('city', $errors[2]->getField());
+            $this->assertEquals('street', $errors[3]->getField());
         }, function ($formData) {
             $this->fail('The form must be invalid here.');
         });
