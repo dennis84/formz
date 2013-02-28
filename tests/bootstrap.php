fa2e3861dc8545e9354fcafbaa9d06d7b98e5900
Dennis Dietrich <d.dietrich84@googlemail.com>
2013-02-28 09:48:43 +0100

    updated composer.json

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,6 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('Formz\Tests', __DIR__ . '/../tests');
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
